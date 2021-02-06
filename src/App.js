67ff9e8d6b2d72fcd1d96ef1f76dd153257b7684
hannbnguyen <baohanmimi@gmail.com>
2021-02-06 13:45:28 -0500

    fix glitch in darkmode

--- src/App.js
+++ src/App.js
@@ -12,24 +12,22 @@ function App() {
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
   //Darkmode toggle options
+  new Darkmode().showWidget();
   const options = {
-    bottom: '32px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
+    bottom: '64px', // default: '32px'
+    right: '64px', // default: '32px'
+    left: 'unset', // default: 'unset'
     time: '0.5s', // default: '0.3s'
     mixColor: '#fff', // default: '#fff'
     backgroundColor: '#fff',  // default: '#fff'
     buttonColorDark: '#100f2c',  // default: '#100f2c'
     buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: true, // default: true,
+    saveInCookies: false, // default: true,
     label: '🌓', // default: ''
     autoMatchOsTheme: true // default: true
   }
 
-  new Darkmode(). showWidget();
   const darkmode = new Darkmode(options);
-  darkmode.showWidget();
-
   //Run Once when the app starts
   useEffect(() => {
     getLocalTodos();
